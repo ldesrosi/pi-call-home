589e02e4b25b19f24bef62a4576087e764aa1648
ldesrosi <ldesrosi@uk.ibm.com>
2016-11-20 20:58:45 +0000

    Avoid changing rc.local comments

--- setup.sh
+++ setup.sh
@@ -14,9 +14,9 @@ chmod 0755 /opt/pi-call-home
 cp ./publishIP.sh /opt/pi-call-home
 chmod 0755 /opt/pi-call-home/publishIP.sh
 
-command="\(sleep 30\; sudo \/opt\/pi-call-home\/publishIP.sh\ $1 $2)\&"
+command="\(sleep 30\; \/opt\/pi-call-home\/publishIP.sh\ $1 $2)\&"
 
-sed -i.bak 's/exit 0/'"$command"'\n\nexit 0\n/' /etc/rc.local 
+sed -i.bak '/^exit 0/s/exit 0/'"$command"'\n\nexit 0\n/' /etc/rc.local 
 
 echo "root:$2:$3\n" > /etc/ssmtp/revaliases
 
