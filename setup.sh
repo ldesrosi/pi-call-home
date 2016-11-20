afec56f41a30d8e2a92eabb493e43791fa6e051d
ldesrosi <ldesrosi@uk.ibm.com>
2016-11-20 21:10:40 +0000

    Added ssmtp.conf configuration

--- setup.sh
+++ setup.sh
@@ -18,5 +18,13 @@ command="\(sleep 30\; \/opt\/pi-call-home\/publishIP.sh\ $1 $2)\&"
 
 sed -i.bak '/^exit 0/s/exit 0/'"$command"'\n\nexit 0\n/' /etc/rc.local 
 
+sed -i.bak '/^mailhub=mail/s/mailhub=mail/mailhub='"$3"'/' /etc/ssmtp/ssmtp.conf
+
+echo "UseTLS=Yes" >> /etc/ssmtp/ssmtp.conf
+echo "UseSTARTTLS=Yes" >> /etc/ssmtp/ssmtp.conf
+echo "AuthUser=$1" >> /etc/ssmtp/ssmtp.conf
+echo "AuthPass=$4" >> /etc/ssmtp/ssmtp.conf
+
+
 echo "root:$2:$3\n" > /etc/ssmtp/revaliases
 
